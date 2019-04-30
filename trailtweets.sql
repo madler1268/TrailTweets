e7a2d3733210bb934c260b4a67d38c51a1c14ac4
madler1268 <madler12@vt.edu>
2019-04-30 17:15:37 -0400

    More SQL

--- trailtweets.sql
+++ trailtweets.sql
@@ -46,13 +46,13 @@ CREATE TABLE `tweets` (
 --
 
 INSERT INTO `tweets` (`TweetID`, `account_name`, `username`, `tweet_text`, `image_link`, `likes`, `retweets`, `date`, `location`, `tag`) VALUES
-(1, '@tony551', 'Best Hiker!', 'This place is amazing', 'apptrail.JPG', 55, 23, 'April 15, 2017', 'Virginia', 'nature'),
+(1, '@tony551', 'Best Hiker!', 'This place is amazing', 'apptrail.JPG', 200, 23, 'April 15, 2017', 'Virginia', 'nature'),
 (2, '@Hiker994', 'Hike all Day', 'Georgias views are the best there are', 'georgia2.jpg', 200, 100, 'April 17, 2017', 'Virginia', 'nature'),
 (3, '@tony551', 'Best Hiker!', 'This place is amazing', 'apptrail.JPG', 55, 23, 'April 15, 2017', 'Virginia', 'weather'),
 (4, '@Hiker994', 'Hike all Day', 'Georgias views are the best there are', 'georgia2.jpg', 200, 100, 'April 17, 2017', 'Virginia', 'packing'),
 (5, '@tony551', 'Best Hiker!', 'This place is amazing', 'apptrail.JPG', 55, 23, 'April 15, 2017', 'Virginia', 'firstaid'),
 (6, '@Hiker994', 'Hike all Day', 'Georgias views are the best there are', 'georgia2.jpg', 200, 100, 'April 17, 2017', 'Georgia', 'weather'),
-(7, '@tony551', 'Best Hiker!', 'This place is amazing', 'apptrail.JPG', 55, 23, 'April 15, 2017', 'Virginia', 'weather'),
+(7, '@tony551', 'Best Hiker!', 'This place is amazing', 'apptrail.JPG', 55, 23, 'April 15, 2017', 'Virginia', 'planning'),
 (8, '@Hiker994', 'Hike all Day', 'Georgias views are the best there are', 'georgia2.jpg', 200, 100, 'April 17, 2017', 'New York', 'packing'),
 (9, '@tony551', 'Best Hiker!', 'This place is amazing', 'georgia3.jpg', 55, 23, 'April 15, 2017', 'Virginia', 'wildlife'),
 (10, '@Hiker994', 'Hike all Day', 'Georgias views are the best there are', 'georgia2.jpg', 200, 100, 'April 17, 2017', 'Virginia', 'packing'),
@@ -61,7 +61,12 @@ INSERT INTO `tweets` (`TweetID`, `account_name`, `username`, `tweet_text`, `imag
 (13, '@tony551', 'Best Hiker!', 'This place is amazing', 'mcafeeknob.JPG', 123, 444, 'April 15, 2017', 'Virginia', 'planning'),
 (14, '@VictorHayes', 'Victor', 'Hike of the week!', 'bigelow-mountains.jpg', 10, 2, 'April 17, 2017', 'New Hampshire', 'family'),
 (15, '@TonyMaloney', 'Owl', 'Found a great buddy', 'tm-img-14-tn.jpg', 10, 2, 'April 23, 2017', 'Vermont', 'wildlife'),
-(16, '@HarryTerry', 'Mystery Bird', 'Anyone know what bord this is?', 'tm-img-12-tn.jpg', 200, 100, 'April 7, 2017', 'Georgia', 'wildlife');
+(16, '@HarryTerry', 'Mystery Bird', 'Anyone know what bord this is?', 'tm-img-12-tn.jpg', 75, 14, 'April 7, 2017', 'Georgia', 'wildlife'),
+(17, '@HarryTerry', 'Keffer Oak', 'I love this tree!', 'kefferoak.jpg', 85, 34, 'March 7, 2017', 'Virginia', 'wildlife'),
+(18, '@HarryTerry', 'McAfee Knob', 'Finally made it!', 'mcafeeknob.jpg', 354, 175, 'May 19, 2017', 'Virginia', 'nature'),
+(19, '@DobertRowneySr', 'Fox', 'How cozy I feel right now', 'tm-img-16-tn.jpg', 270, 86, 'February, 2017', 'Virginia', 'weather'),
+(20, '@KreeLarson', 'White Fox', 'This is how cold it is', 'tm-img-07-tn.jpg', 115, 61, 'February, 2017', 'Maine', 'weather');
+(20, '@AvocadoMost', 'Yearly Hike', 'Love these guys', 'georgia1.jpg', 43, 5, 'June 10, 2017', 'Maryland', 'family');
 
 -- --------------------------------------------------------
 
